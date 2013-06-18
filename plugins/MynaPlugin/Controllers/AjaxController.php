b0f7a2b712a00c4a2d8746efbad6fb0519450866
david grupp <david@grupp.me>
2013-06-17 19:32:04 -0500

    made controllers load correctly

--- plugins/MynaPlugin/Controllers/AjaxController.php
+++ plugins/MynaPlugin/Controllers/AjaxController.php
@@ -9,7 +9,11 @@ require_once($root.'/wp-content/plugins/MynaPlugin/Utility/Utility.php');
 class AjaxController extends BaseController{
 
 	public function Actions(){
-
+		$currentUrl =$_SERVER['REQUEST_URI'];
+		$action = 'view';
+		if(isset($_REQUEST['a']) && false == empty($_REQUEST['a'])){
+			$action = $_REQUEST['a'];
+		}
 	}
 	
 }
--- plugins/MynaPlugin/Controllers/MediaController.php
+++ plugins/MynaPlugin/Controllers/MediaController.php
@@ -5,10 +5,14 @@ require_once($root.'/wp-content/plugins/MynaPlugin/Models/MediaModels.php');
 require_once($root.'/wp-content/plugins/MynaPlugin/Views/MediaViews.php');
 require_once($root.'/wp-content/plugins/MynaPlugin/Utility/Utility.php');
 
-class EventsController extends BaseController{
+class MediaController extends BaseController{
 
 	public function Actions(){
-		
+		$currentUrl =$_SERVER['REQUEST_URI'];
+		$action = 'view';
+		if(isset($_REQUEST['a']) && false == empty($_REQUEST['a'])){
+			$action = $_REQUEST['a'];
+		}
 	}
 }
 
